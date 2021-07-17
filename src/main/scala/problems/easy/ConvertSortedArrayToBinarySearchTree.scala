3ccedd8ba2f412e1004f5fca49fb76e6ff8de66f
Vyun Sergey <vyun.sergey@icloud.com>
2021-07-17 13:02:59 +0300

    add Convert Sorted Array to Binary Search Tree solution

---
+++ src/main/scala/problems/easy/ConvertSortedArrayToBinarySearchTree.scala
@@ -0,0 +1,43 @@
+package problems.easy
+
+object ConvertSortedArrayToBinarySearchTree {
+
+  class TreeNode(_value: Int = 0,
+                 _left: TreeNode = null,
+                 _right: TreeNode = null) {
+    var value: Int = _value
+    var left: TreeNode = _left
+    var right: TreeNode = _right
+  }
+
+  def main(args: Array[String]): Unit = {
+    val nums: Array[Int] = Console.in.readLine().split(" ").map(_.toInt)
+    val res: TreeNode = sortedArrayToBST(nums)
+    println(printTree(res))
+  }
+
+  def printTree(node: TreeNode): String = {
+    if (node == null) {
+      "TreeNode()"
+    } else if (node.left == null && node.right == null) {
+      s"TreeNode(v=${node.value})"
+    } else {
+      s"TreeNode(v=${node.value} l=${printTree(node.left)} r=${printTree(node.right)})"
+    }
+  }
+
+  def sortedArrayToBST(nums: Array[Int]): TreeNode = {
+    val len = nums.length
+    if (len > 1) {
+      val mid = len / 2
+      new TreeNode(
+        nums(mid),
+        sortedArrayToBST(nums.take(mid)),
+        sortedArrayToBST(nums.drop(mid + 1))
+      )
+    } else if (len == 1) {
+      new TreeNode(nums.head)
+    } else null
+  }
+
+}
