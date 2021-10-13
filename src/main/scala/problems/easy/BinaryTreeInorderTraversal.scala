1255149b6d0cd01000378f7eaf3308f29fd7b0d9
VyunSergey <vyun.sergey@icloud.com>
2021-10-13 15:37:50 +0300

    add Binary Tree Inorder Traversal solution

---
+++ src/main/scala/problems/easy/BinaryTreeInorderTraversal.scala
@@ -0,0 +1,19 @@
+package problems.easy
+
+object BinaryTreeInorderTraversal {
+  def main(args: Array[String]): Unit = {
+    val tree = TreeNode(1,
+      TreeNode(2,
+        TreeNode(4, TreeNode(8), TreeNode(9)), TreeNode(5)
+      ), TreeNode(3,
+        TreeNode(6), TreeNode(7, null, TreeNode(10))
+      ))
+    val res = inorderTraversal(tree)
+    println(res)
+  }
+
+  def inorderTraversal(root: TreeNode): List[Int] = {
+    if (root == null) Nil
+    else inorderTraversal(root.left) ++ List(root.value) ++ inorderTraversal(root.right)
+  }
+}
