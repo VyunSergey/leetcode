fe06b3b48a950b2cfd9642dd39e72079bf0cea06
Vyun Sergey <vyun.sergey@icloud.com>
2022-05-21 11:17:23 +0300

    add solution for Kth Largest Element in an Array

--- src/main/scala/problems/medium/KLargestElementOfArray.scala
+++ src/main/scala/problems/medium/KLargestElementOfArray.scala
@@ -1,5 +1,7 @@
 package problems.medium
 
+import scala.collection.mutable
+
 object KLargestElementOfArray {
   def main(args: Array[String]): Unit = {
     val nums: Array[Int] = Console.in.readLine().split(" ").map(_.toInt)
@@ -11,5 +13,15 @@ object KLargestElementOfArray {
   def findKthLargest(nums: Array[Int], k: Int): Int = {
     // println(nums.sorted.toList)
     nums.sorted.apply(nums.length - k)
+  }
+
+  def findKthLargestTwo(nums: Array[Int], k: Int): Int = {
+    val queue = mutable.PriorityQueue.empty[Int]
+    var res = -1
+    queue ++= nums
+    (0 until k).foreach { _ =>
+      res = queue.dequeue
+    }
+    res
   }
 }
