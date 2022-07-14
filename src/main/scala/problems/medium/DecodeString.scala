8c34662b966686df07c8a53ca361d44bd3c447d6
Vyun Sergey <vyun.sergey@icloud.com>
2022-07-14 14:11:56 +0300

    add solution for Decode String using Stack

--- src/main/scala/problems/medium/DecodeString.scala
+++ src/main/scala/problems/medium/DecodeString.scala
@@ -10,6 +10,51 @@ object DecodeString {
   }
 
   def decodeString(s: String): String = {
+    val n = s.length
+    val stack = mutable.Stack.empty[(String, Int)]
+    var brackets = 0
+    var i = 0
+    var cur = ""
+    var res = ""
+
+    while(i < n) {
+      // println(s"i=$i s($i)=${s(i)} stack=$stack")
+      if (s(i).isDigit) {
+        val sb = new mutable.StringBuilder()
+        while(i < n && s(i).isDigit) {
+          sb += s(i)
+          i += 1
+        }
+        stack.push((cur, sb.result.toInt))
+        cur = ""
+      } else if (s(i) == '[') {
+        brackets += 1
+        i += 1
+      } else if (s(i).isLetter) {
+        val sb = new mutable.StringBuilder()
+        while(i < n && s(i).isLetter) {
+          sb += s(i)
+          i += 1
+        }
+        cur += sb.result
+      } else if (s(i) == ']') {
+        val (str, num) = stack.pop()
+        cur = str + cur * num
+        brackets -= 1
+        if (brackets == 0) {
+          res += cur
+          cur = ""
+          // println(s"res=$res")
+        }
+        i += 1
+      }
+    }
+    // println(s"res=$res cur=$cur brackets=$brackets stack=$stack")
+
+    res + cur
+  }
+
+  def decodeString2(s: String): String = {
     var stk = 0
     var str1 = ""
     var str2 = ""
@@ -48,7 +93,7 @@ object DecodeString {
     // println(arr.toList)
     arr.map { case (i, str) =>
       if (i < 0) str
-      else if (str.exists(List('[', ']').contains(_))) decodeString(str) * i
+      else if (str.exists(List('[', ']').contains(_))) decodeString2(str) * i
       else str * i
     }.reduce(_ + _)
   }
